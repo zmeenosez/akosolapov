8fc91bfe99d3726ffa83a794ba285d13dfc8cd2b
zmeenosez <alex.zmeenosez@gmail.com>
2017-02-23 20:45:27 +0300

    junit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,16 @@
 package ru.job4j;
-/**
-*класс Calculate решение задачи урока 1.1 части 001.
-*@author Aleksey Kosolapov.
-*@since 11.02.17.
-*@version 0.1.
-*/
 
+/**
+ * класс Calculate решение задачи урока 1.1 части 001.
+ * @author Aleksey Kosolapov.
+ * @since 11.02.17.
+ * @version 0.1.
+ */
 public class Calculate {
 	/**
-	* метод main.Вывод строки в консоль.
-	*@param  args - args.
-	*/	
+	 * метод main.Вывод строки в консоль.
+	 * @param args - args.
+	 */	
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
