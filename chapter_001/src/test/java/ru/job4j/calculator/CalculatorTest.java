82abe0dd5f23457117606363aff5c8b6ff644143
zmeenosez <alex.zmeenosez@gmail.com>
2017-03-12 20:57:04 +0300

    Тест элементарный калькулятор

---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Aleksey Kosolapov.
+ * @since 12.03.17.
+ * @version 0.1.
+ */
+
+public class CalculatorTest {
+	/**
+	 * Test add,substruct,div,multiple.
+	 */
+	 @Test
+    public void whenAddOnePlusOneThenTwo() { //сложение
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }	  
+	 @Test
+    public void whenAddOnePlusOneThenTwo() { //вычитание
+        Calculator calc = new Calculator();
+        calc.substruct(9D, 2D);
+        double result = calc.getResult();
+        double expected = 7D;
+        assertThat(result, is(expected));
+    }
+	 @Test
+    public void whenAddOnePlusOneThenTwo() { //деление
+        Calculator calc = new Calculator();
+        calc.div(8D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenAddOnePlusOneThenTwo() { //умножение
+        Calculator calc = new Calculator();
+        calc.multiple(3D, 4D);
+        double result = calc.getResult();
+        double expected = 12D;
+        assertThat(result, is(expected));
+    }
+}
