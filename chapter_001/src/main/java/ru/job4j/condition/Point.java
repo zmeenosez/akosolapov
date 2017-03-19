4c9b78fa0b72973237324e99bc7a496311fd19b3
zmeenosez <alex.zmeenosez@gmail.com>
2017-03-19 21:51:18 +0300

    с ошибками

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,55 @@
+package ru.job4j.condition;
+
+/**
+ *
+ *
+ * @author Aleksey Kosolapov.
+ * @version 0.1.
+ * @since 19.03.17.
+ */
+ public class Point {
+   /**
+	*
+	*/
+   private int x;
+   /**
+	*
+	*/
+   private int y;
+   /**
+	*
+	* @param x number.
+	* @param y number.
+	*/
+   public  Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+  }
+   /**
+	*
+	* @return this.x.
+	*/
+  public int getX() {
+      return this.x;
+  }
+   /**
+	*
+	* @return this.y.
+	*/
+  public int getY() {
+     return this.y;
+  }
+   /**
+	*
+	* @param a number.
+	* @param b number.
+	* @return true.
+	*/
+  public boolean is(int a, int b) {
+	   int y1 = a * x + b;
+	   if (y == y1) {
+	   y = a * x + b;
+	   }
+       return true;
+  }
+}
