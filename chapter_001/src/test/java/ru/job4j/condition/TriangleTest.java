a21bbbe21a711956ab100d1b5b71486ea115c046
zmeenosez <alex.zmeenosez@gmail.com>
2017-03-27 23:58:35 +0300

    calculate the triangle area

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -12,18 +12,21 @@ import static org.junit.Assert.assertThat;
  * @since 23.03.17.
  * @version 0.1.
  */
- 
+
  public class TriangleTest {
 	/**
 	 * Test calculate the triangle.
 	 */
 	 @Test
-    public void whenresultTwotoOnetoTwotwenty-fourhundredthsThenZeroninety-ninehundredths () {
-		Triangle coordinat = new Triangle( 2,3; 4,3; 4,4);
-		double result = coordinat.area( 2; 1; 2,24 );
-		double except = 0,9999;
-		assertThat(result, closeTo(except, 0.9999));
+    public void whenresultTwotoOnetoTwotwentyfourhundredthsThenZeroninetyninehundredths () {
+		Point a = new Point(2, 3);
+        Point b = new Point(4, 3);
+        Point c = new Point(4, 4);
+		Triangle coordinat = new Triangle( a, b, c );
+		double result = coordinat.area( 2, 1, 2,24 );
+		double except = 0,99;
+		assertThat(result, closeTo(except, 0.99));
 	}
  }
- 
- 
+
+
