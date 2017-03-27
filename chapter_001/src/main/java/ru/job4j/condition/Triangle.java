f6fe461aadee29f483e223d69da495807df17f14
zmeenosez <alex.zmeenosez@gmail.com>
2017-03-27 20:42:01 +0300

    площадь треугольника

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,59 @@
+package ru.job4j.condition;
+
+/**
+ * Вычисление площади треугольника.
+ *
+ * @author Aleksey Kosolapov.
+ * @version 0.1.
+ * @since 23.03.17.
+ */
+ public class Triangle {
+ private Point a;
+ private Point b;
+ private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    }
+   /**
+	* Calculate the triangle area.
+	* @param a number.
+	* @param b number.
+	* @param c number.
+	*
+	*/
+    public double area() {
+    double absideLength = a.sideLength(b);//сторона от a точки до b точки.
+    double bcsideLength = b.sideLength(c);//сторона от b точки до c точки.
+    double casideLength = c.sideLength(a);//сторона от c точки до a точки.
+	/**
+     *вычисление сторон треугольника.
+     */
+	double sideLength(Point coordinat) {
+    return Math.sqrt ( (Math.pow( (point.x - this.x),2))+(Math.pow( (point.y - this.y),2)));
+    }
+    /** определяем условие существования треугольника.
+     */
+    if ((absideLength + bcsideLength) > casideLength ||
+	(absideLength + casideLength) > bcsideLength ||
+	(bcsideLengthb + casideLength) > absideLength) {
+		/**
+		 * вычисляем полупериметр треугольника.
+		 */
+        double p = ((absideLength + bcsideLength + casideLength) / 2);
+		/**
+		 * вычисляем площадь треугольника.
+		 */
+		double s = (Math.sqrt(p * (p - absideLength) * (p - bcsideLength) * (p - casideLength)));
+        }
+        else { false //треугольник не существует.
+        }
+    return double s;
+ }
+
+
+
+
+
