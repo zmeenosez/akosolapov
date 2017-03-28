cd28b723d2ff9a947ecb1d6880998cdb20d34b8d
zmeenosez <alex.zmeenosez@gmail.com>
2017-03-28 22:21:32 +0300

    площадь треугольника

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -8,37 +8,50 @@ package ru.job4j.condition;
  * @since 23.03.17.
  */
  public class Triangle {
+ /**
+  * Поле с значением точки a.
+  */
  private Point a;
+ /**
+  * Поле с значением точки b.
+  */
  private Point b;
+ /**
+  * Поле с значением точки c.
+  */
  private Point c;
-
+   /**
+    * Метод обращающийся к rлассу Point c координатами точек и присваивающий полям значения.
+	* @param a number.
+	* @param b number.
+	* @param c number.
+	*/
     public Triangle(Point a, Point b, Point c) {
     this.a = a;
     this.b = b;
     this.c = c;
     }
 	/**
-     *вычисление сторон треугольника.
+     * вычисление сторон треугольника.
+	 * @return sideLength.
+	 * @param coordinat number.
      */
 	double sideLength(Point coordinat) {
-    return Math.sqrt ( (Math.pow( (point.x - this.x),2))+(Math.pow( (point.y - this.y),2)));
+    return Math.sqrt((Math.pow((coordinat.x - this.x), 2)) + (Math.pow((coordinat.y - this.y), 2)));
     }
    /**
 	* Calculate the triangle area.
-	* @param a number.
-	* @param b number.
-	* @param c number.
-	*
+	* @return s.
 	*/
     public double area() {
-    double absideLength = a.sideLength(b);//сторона от a точки до b точки.
-    double bcsideLength = b.sideLength(c);//сторона от b точки до c точки.
-    double casideLength = c.sideLength(a);//сторона от c точки до a точки.
+    double absideLength = a.sideLength(b); //сторона от a точки до b точки.
+    double bcsideLength = b.sideLength(c); //сторона от b точки до c точки.
+    double casideLength = c.sideLength(a); //сторона от c точки до a точки.
 	/** определяем условие существования треугольника.
      */
-    if ((absideLength + bcsideLength) > casideLength ||
-	(absideLength + casideLength) > bcsideLength ||
-	(bcsideLengthb + casideLength) > absideLength) {
+    if ((absideLength + bcsideLength) > casideLength
+	|| (absideLength + casideLength) > bcsideLength
+	|| (bcsideLengthb + casideLength) > absideLength) {
 		/**
 		 * вычисляем полупериметр треугольника.
 		 */
@@ -47,10 +60,10 @@ package ru.job4j.condition;
 		 * вычисляем площадь треугольника.
 		 */
 		double s = (Math.sqrt(p * (p - absideLength) * (p - bcsideLength) * (p - casideLength)));
+        } else {
+		double s = 0; //треугольник не существует.
         }
-        else { double s = 0; //треугольник не существует.
-        }
-    return s;
+        return s;
 	}
  }
 
